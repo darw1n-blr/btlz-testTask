81cabdddddd51f38839fc1bb6404d974ecb8d10a
darw1n-blr <egorogovcov@gmail.com>
2024-11-12 00:14:49 +0300

    delete unused imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,9 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Controller, Post} from '@nestjs/common';
 import { AppService } from './app.service';
-import {AddCoefDto} from "./knex/dto/addCoef.dto";
-import {KnexService} from "./knex/knex.service";
-import {GoogleSheetsService} from "./google-sheets.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-              private readonly knexService: KnexService,
-              private readonly googleSheetsService: GoogleSheetsService) {}
+  constructor(private readonly appService: AppService) {}
 
 
     @Post('update')
